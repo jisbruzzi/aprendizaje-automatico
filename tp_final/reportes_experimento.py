dae076a4f39d417b99ab98e1271ec692f1b2eba1
jisbruzzi <jose.sbru@gmail.com>
2019-03-10 20:46:26 -0300

    Ultima modificacion para el jueves

--- tp_final/reportes_experimento.py
+++ tp_final/reportes_experimento.py
@@ -4,7 +4,12 @@ import sys
 import numpy as np
 from scipy.optimize import minimize
 
-experimentos=pickle.load(open(sys.argv[1],"rb"))
+from pathlib import Path
+archivos=Path(sys.argv[1]).glob("*.pickle")
+contenido = map(lambda f:pickle.load(f.open("rb")) ,archivos)
+experimentos=sum(contenido,[])
+
+ds = sorted(list(set(map(lambda x:x["d"], experimentos))))
 
 def c_para_coordenadas(k,n,e):
     return e*(n**k)
@@ -39,6 +44,18 @@ def n_e_para_dimension(d):
         )
         for experimento in experimentos if experimento["d"]==d
     ])
+def mostrar_busqueda_k_para_d(d,k_elegido):
+    ns,errores_l2=n_e_para_dimension(d)
+    funcional=hacer_funcional_busqueda_k(ns,errores_l2)
+    xs=np.arange(-1,1,0.001)
+    ys=[funcional(x) for x in xs]
+    plt.title("busqueda de k para d={}. K elegido:{}".format(d,k_elegido))
+    plt.xlabel("k")
+    plt.yscale("log")
+    plt.ylabel("diferencia cuadrada")
+    plt.plot(xs,ys)
+    plt.savefig( (Path(sys.argv[1])/"figuras"/"busqueda-de-k-d={}.png".format(d)).open("wb") )
+    plt.clf()
 
 def calcular_k_c_cideal_para_d(d):
     ns,errores_l2=n_e_para_dimension(d)
@@ -52,12 +69,16 @@ def calcular_k_c_cideal_para_d(d):
         return c_ideal*(x**(-k_ideal))
     def funcion_real(x):
         return c*(x**(-k))
-    return np.asscalar(k), np.asscalar(c), funcional(k), c_ideal, funcion_ideal, funcion_real
+    return np.asscalar(k), np.asscalar(c), funcional(k), c_ideal, funcion_ideal, funcion_real,k[0]
 
-ds=[1,2,3,4,6,8,10]
-ks,cs,es,c_ideales,funciones_ideales,funciones_reales = zip(*[
+
+
+ks,cs,es,c_ideales,funciones_ideales,funciones_reales,k_reales = zip(*[
     calcular_k_c_cideal_para_d(d) for d in ds
 ])
+for d,k in zip(ds,k_reales):
+    mostrar_busqueda_k_para_d(d,k)
+
 print(ds)
 print("----")
 print(2.0/(np.asarray(ds)+2.0))
@@ -77,23 +98,27 @@ plt.legend()
 plt.ylabel("Error estimado")
 plt.xlabel("n")
 plt.title("Promedio del error estimado variando n y d")
-plt.savefig("figuras/resultados-grales.png")
+
+if not (Path(sys.argv[1])/"figuras").exists:
+    (Path(sys.argv[1])/"figuras").mkdir()
+
+plt.savefig( (Path(sys.argv[1])/"figuras"/"resultados-grales.png").open("wb") )
 plt.clf()
 
-def grafico_para(d,f_i,f_r):
+def grafico_para(d,f_i,f_r,k):
     n,e=n_e_para_dimension(d)
     plt.plot(n,e,label="error real")
-    plt.plot(n,f_i(np.asarray(n)),label="cota con k teórico")
-    plt.plot(n,f_r(np.asarray(n)),label="menor cota con un k distinto del teórico")
+    plt.plot(n,f_i(np.asarray(n)),label="cota con k=d/(d+2)={}".format(str(float(d)/(float(d)+2))))
+    plt.plot(n,f_r(np.asarray(n)),label="menor cota con k={}".format(str(k)))
     plt.title("Promedio del error experimental y cotas teóricas para d={}".format(d))
     plt.xlabel("n")
     plt.ylabel("Promedio del error estimado")
     plt.legend()
-    plt.savefig("figuras/cotas-error-d={}.png".format(d))
+    plt.savefig(  (Path(sys.argv[1])/"figuras"/("cotas-error-d={}.png".format(d))).open("wb") )
     plt.clf()
 
-for d, f_i, f_r in zip(ds,funciones_ideales,funciones_reales):
-    grafico_para(d,f_i,f_r)
+for d, f_i, f_r,k in zip(ds,funciones_ideales,funciones_reales,k_reales):
+    grafico_para(d,f_i,f_r,k)
 
 
 
@@ -103,5 +128,5 @@ plt.plot(ds,2/(np.asarray(ds)+2),label="k teórico")
 plt.ylabel("k")
 plt.xlabel("n")
 plt.legend()
-plt.savefig("figuras/k-variando-d.png")
+plt.savefig( (Path(sys.argv[1])/"figuras"/"k-variando-d.png").open("wb") )
 plt.clf()
